56d7bad056fd5adc5578cc6858e99778e8c853f5
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-07-13 18:26:36 +0630

    fix: handle empty cart in addToCartAction and initialize cart properly

--- src/lib/utils/cached.js
+++ src/lib/utils/cached.js
@@ -1,12 +1,12 @@
-import { redis } from '@/lib/redis';
+import { redis } from "@/lib/redis";
 
 export async function getCachedData(key) {
   const data = await redis.get(key);
-  return data ;
+  return data;
 }
 
 export async function setCachedData(key, value, ttl = 300) {
-  await redis.set(key, JSON.stringify(value), { EX: ttl }); // for ioredis
+  await redis.set(key, JSON.stringify(value), { ex: ttl }); // for ioredis
 }
 
 export async function delKey(key) {
