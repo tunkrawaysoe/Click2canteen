bbe70820cfad268aeda2d4495b4ac4bb96f64669
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-07-19 21:33:52 +0630

    Fix

--- src/app/api/auth/creation/route.js
+++ src/app/api/auth/creation/route.js
@@ -1,37 +1,33 @@
+
 import prisma from "@/lib/prisma";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
+import { NextResponse } from "next/server";
 
-export async function GET() {
-  // Get user from Kinde server session
+export async function GET(request) {
   const { getUser } = getKindeServerSession();
   const kindeUser = await getUser();
 
-  // If user is not authenticated or missing data, redirect to login
   if (!kindeUser?.id || !kindeUser?.email || !kindeUser?.given_name) {
-    return redirect("/login");
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // Check if user already exists in the DB
   const existingUser = await prisma.user.findUnique({
     where: { id: kindeUser.id },
   });
 
-  // If not, create the new user in the DB
- if (!existingUser) {
-  await prisma.user.create({
-    data: {
-      id: kindeUser.id,
-      name: `${kindeUser.given_name} ${kindeUser.family_name}`, 
-      email: kindeUser.email,
-      profileImage: kindeUser.picture ?? null,
-      role: "CUSTOMER",
-    },
-  });
-}
+  if (!existingUser) {
+    await prisma.user.create({
+      data: {
+        id: kindeUser.id,
+        name: `${kindeUser.given_name}${
+          kindeUser.family_name ? " " + kindeUser.family_name : ""
+        }`,
+        email: kindeUser.email,
+        profileImage: kindeUser.picture || null,
+        role: "CUSTOMER",
+      },
+    });
+  }
 
-  
-
-  // Redirect to home page after login or signup
-  return redirect("/");
+  return NextResponse.redirect(new URL("/", request.url));
 }
