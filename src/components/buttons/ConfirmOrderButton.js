71de14e196106cf6c6f5a1c4d7c3e4b69aa3e392
tunkrawaysoe <57566289+tunkrawaysoe@users.noreply.github.com>
2025-07-25 22:10:33 +0630

    Fix

--- src/components/buttons/ConfirmOrderButton.js
+++ src/components/buttons/ConfirmOrderButton.js
@@ -1,13 +1,11 @@
 "use client";
 
 import { placeOrder } from "@/actions/cart";
-import { useFormStatus } from "react-dom";
-import { Button, CircularProgress } from "@mui/material";
 import SubmitButton from "./SubmitButton";
 
 export default function ConfirmOrderButton({ userId }) {
   return (
-    <form action={placeOrder} method="post">
+    <form action={placeOrder}>
       <input type="hidden" name="userId" value={userId} />
       <SubmitButton label="Confirm Order" />
     </form>
